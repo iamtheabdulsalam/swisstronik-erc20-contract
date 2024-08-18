fc44cb03b18c95384415656db5a35f70b1dc04e9
Abdulsalam Abdulsalam <0xdudul@gmail.com>
2024-08-18 14:26:56 +0000

    feat: implemented erc20 mint contract

---
+++ auto.bash
@@ -0,0 +1,32 @@
+npm i
+npm run compile
+
+npm run deploy
+npm run deploy
+npm run deploy
+npm run deploy
+
+npm run mint
+npm run mint
+npm run mint
+npm run mint
+npm run mint
+
+npm run check-supply
+
+npm run transfer
+npm run transfer
+npm run transfer
+npm run transfer
+npm run transfer
+npm run transfer
+npm run transfer
+npm run transfer
+npm run transfer
+npm run transfer
+npm run transfer
+
+rm -rf .git
+git init
+git add .
+git commit -m "feat: implemented erc20 mint contract"
---
+++ utils/deployed-address.ts
@@ -0,0 +1,3 @@
+const deployedAddress = '0x29C49113bB3884b94Bd55b0c68eD9EE1A0913359'
+
+export default deployedAddress
